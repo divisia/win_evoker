e8421161d32f2db4c2d7e095b3ce34086d7e3226
divisia <47565842+divisia@users.noreply.github.com>
2019-04-21 15:41:46 +0300

    Add files via upload

---
+++ Source Code/Graph/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Graph
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radTek = new System.Windows.Forms.RadioButton();
+            this.radCift = new System.Windows.Forms.RadioButton();
+            this.radDort = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPunto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbFont = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMetin = new System.Windows.Forms.TextBox();
+            this.btnSSV = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.backColour = new System.Windows.Forms.ColorDialog();
+            this.frontColour = new System.Windows.Forms.ColorDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(304, 231);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.txtMetin);
+            this.tabPage1.Controls.Add(this.btnSSV);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(288, 202);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "SUPER SIMPLE ONE";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radTek);
+            this.groupBox2.Controls.Add(this.radCift);
+            this.groupBox2.Controls.Add(this.radDort);
+            this.groupBox2.Location = new System.Drawing.Point(199, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(84, 88);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Functional";
+            // 
+            // radTek
+            // 
+            this.radTek.AutoSize = true;
+            this.radTek.Location = new System.Drawing.Point(6, 19);
+            this.radTek.Name = "radTek";
+            this.radTek.Size = new System.Drawing.Size(65, 17);
+            this.radTek.TabIndex = 6;
+            this.radTek.TabStop = true;
+            this.radTek.Text = "One text";
+            this.radTek.UseVisualStyleBackColor = true;
+            // 
+            // radCift
+            // 
+            this.radCift.AutoSize = true;
+            this.radCift.Location = new System.Drawing.Point(6, 42);
+            this.radCift.Name = "radCift";
+            this.radCift.Size = new System.Drawing.Size(76, 17);
+            this.radCift.TabIndex = 7;
+            this.radCift.TabStop = true;
+            this.radCift.Text = "Doubletext";
+            this.radCift.UseVisualStyleBackColor = true;
+            // 
+            // radDort
+            // 
+            this.radDort.AutoSize = true;
+            this.radDort.Location = new System.Drawing.Point(7, 65);
+            this.radDort.Name = "radDort";
+            this.radDort.Size = new System.Drawing.Size(77, 17);
+            this.radDort.TabIndex = 8;
+            this.radDort.TabStop = true;
+            this.radDort.Text = "Quadrotext";
+            this.radDort.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtPunto);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbFont);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(185, 118);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Visual";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(75, 39);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Choose";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Font";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(75, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Choose";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Punto";
+            // 
+            // txtPunto
+            // 
+            this.txtPunto.Location = new System.Drawing.Point(75, 67);
+            this.txtPunto.Name = "txtPunto";
+            this.txtPunto.Size = new System.Drawing.Size(97, 20);
+            this.txtPunto.TabIndex = 3;
+            this.txtPunto.Text = "56";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Back Color";
+            // 
+            // cmbFont
+            // 
+            this.cmbFont.FormattingEnabled = true;
+            this.cmbFont.Location = new System.Drawing.Point(75, 93);
+            this.cmbFont.Name = "cmbFont";
+            this.cmbFont.Size = new System.Drawing.Size(97, 21);
+            this.cmbFont.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Front Color";
+            // 
+            // txtMetin
+            // 
+            this.txtMetin.Location = new System.Drawing.Point(8, 130);
+            this.txtMetin.Multiline = true;
+            this.txtMetin.Name = "txtMetin";
+            this.txtMetin.Size = new System.Drawing.Size(275, 65);
+            this.txtMetin.TabIndex = 4;
+            this.txtMetin.Text = "YOU MUST WRITE RIGHT HERE";
+            // 
+            // btnSSV
+            // 
+            this.btnSSV.Location = new System.Drawing.Point(199, 97);
+            this.btnSSV.Name = "btnSSV";
+            this.btnSSV.Size = new System.Drawing.Size(84, 27);
+            this.btnSSV.TabIndex = 2;
+            this.btnSSV.Text = "OK";
+            this.btnSSV.UseVisualStyleBackColor = true;
+            this.btnSSV.Click += new System.EventHandler(this.btnSSV_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(296, 205);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "THE ONE COOLS USE";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Wipe";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 127);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(7, 7);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(273, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 231);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Evoker";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radDort;
+        private System.Windows.Forms.RadioButton radCift;
+        private System.Windows.Forms.RadioButton radTek;
+        private System.Windows.Forms.ComboBox cmbFont;
+        private System.Windows.Forms.TextBox txtMetin;
+        private System.Windows.Forms.TextBox txtPunto;
+        private System.Windows.Forms.Button btnSSV;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ColorDialog backColour;
+        private System.Windows.Forms.ColorDialog frontColour;
+    }
+}
+
---
+++ Source Code/Graph/Form1.cs
@@ -0,0 +1,218 @@
+﻿using Evoker;
+using Evoker.Properties;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Graph
+{
+    public partial class MainForm : Form
+    {
+        int scr_x;
+        int scr_y;
+
+        string layoutPath = Path.Combine(Application.StartupPath, "layout.bmp");
+        string backgroundPath = Path.Combine(Application.StartupPath, "background.bmp");
+        string defPath = Path.Combine(Application.StartupPath, "def.bmp");
+        string notesPath = Path.Combine(Application.StartupPath, "notesDb.txt");
+        string[] notesDb;
+
+        List<string> NoteText = new List<string>();
+        PointF[] NoteLocations = new PointF[9];
+        StreamWriter sw;
+        
+
+        public MainForm()
+        {
+            InitializeComponent();
+            scr_x = Screen.PrimaryScreen.Bounds.Width;
+            scr_y = Screen.PrimaryScreen.Bounds.Height;
+            Setup();
+        }
+
+        void Setup()
+        {
+            #region Set Note Locations
+            int x = scr_x / 8;
+            int y = scr_y / 6;
+
+            NoteLocations[0] = new PointF(5 * x, scr_y / 2); //Middle Notes
+            NoteLocations[1] = new PointF(7 * x, scr_y / 2);
+            NoteLocations[2] = new PointF(3 * x, scr_y / 2);
+
+            NoteLocations[3] = new PointF(5 * x, y ); //Upper Notes
+            NoteLocations[4] = new PointF(7 * x, y );
+            NoteLocations[5] = new PointF(3 * x, y );
+
+            NoteLocations[6] = new PointF(5 * x, 5 * y); //Downer Notes
+            NoteLocations[7] = new PointF(7 * x, 5 * y);
+            NoteLocations[8] = new PointF(3 * x, 5 * y);
+
+            #endregion                
+            notesDb = File.ReadAllLines(notesPath);
+            comboBox1.DataSource = notesDb;
+
+            List<string> fonts = new List<string>();
+            foreach (FontFamily font in FontFamily.Families) { fonts.Add(font.Name); }
+            cmbFont.DataSource = fonts;
+
+            frontColour.Color = Color.Black;
+            backColour.Color = Color.White;
+            txtMetin.Text = "Example";
+            txtPunto.Text = "56";
+            cmbFont.SelectedIndex = 1;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            radTek.Checked = true;
+        }
+
+        public void saveAndSet(Image img)
+        {
+            try {
+                if (File.Exists(backgroundPath)) {
+                    try
+                    {
+                        File.Delete(backgroundPath);
+                        img.Save(backgroundPath);
+                        Wallpaper.Set(new Uri(backgroundPath), Wallpaper.Style.Centered);
+                        img.Dispose();
+                        File.Delete(defPath);
+                    }
+                    catch
+                    {
+                        img.Save(defPath);
+                        Wallpaper.Set(new Uri(defPath), Wallpaper.Style.Centered);
+                        img.Dispose();
+                        //File.Delete(backgroundPath);
+                    }
+                }
+                else
+                {
+                    img.Save(backgroundPath);
+                    Wallpaper.Set(new Uri(defPath), Wallpaper.Style.Centered);
+                    File.Delete(defPath);
+                }
+            }
+            catch { MessageBox.Show("Unstability error.", "Err"); }
+        }
+        
+
+        public void UpdateNotes()
+        {
+            Image backgroundImg = Image.FromFile(layoutPath);
+            Graphics graphics = Graphics.FromImage(backgroundImg);
+            SolidBrush solidBrush = new SolidBrush(Color.Black);
+
+            StringFormat stringFormat = new StringFormat
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+
+            NoteText.Clear();
+            NoteText.AddRange(File.ReadAllLines(notesPath));
+            
+            for(int i = 0; i < 9; i++)
+            {
+                graphics.DrawString(NoteText[i], new Font("Arial", 24), solidBrush, NoteLocations[i], stringFormat);
+            }
+
+            saveAndSet(backgroundImg);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = notesDb[comboBox1.SelectedIndex];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.Delete(notesPath);
+            File.AppendAllLines(notesPath, notesDb);
+            UpdateNotes();
+            comboBox1.DataSource = notesDb;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            notesDb[comboBox1.SelectedIndex] = textBox1.Text;
+            comboBox1.DataSource = notesDb;
+        }
+
+        private void btnSSV_Click(object sender, EventArgs e)
+        {
+            Font font = new Font(cmbFont.SelectedItem.ToString(), Convert.ToInt32(txtPunto.Text));
+            Pen pen = new Pen(frontColour.Color);
+
+            font = new Font(cmbFont.SelectedItem.ToString(), Convert.ToInt32(txtPunto.Text));
+            List<string> lines = new List<string>();
+            foreach (string text in txtMetin.Lines) { lines.Add(text); }
+
+            Image img = new Bitmap(scr_x, scr_y);
+            Graphics graphics = Graphics.FromImage(img);
+            graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            graphics.Clear(backColour.Color);
+
+            pen = new Pen(frontColour.Color);
+            pen.Width = Convert.ToInt32(txtPunto.Text) / 8;
+
+            StringFormat sf = new StringFormat
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+
+            if (radTek.Checked)
+            {
+                graphics.DrawString(lines[0], font, new SolidBrush(frontColour.Color), scr_x / 2, scr_y / 2, sf);
+            }
+            else if (radCift.Checked)
+            {
+                try
+                {
+                    graphics.DrawLine(pen, scr_x / 2, 0, scr_x / 2, scr_y);
+                    graphics.DrawString(lines[0], font, new SolidBrush(frontColour.Color), scr_x / 4, scr_y / 2, sf);
+                    graphics.DrawString(lines[1], font, new SolidBrush(frontColour.Color), scr_x * 3 / 4, scr_y / 2, sf);
+                }
+                catch
+                {
+                    MessageBox.Show("Insufficient input.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (radDort.Checked)
+            {
+                try
+                {
+                    graphics.DrawLine(pen, scr_x / 2, 0, scr_x / 2, scr_y);
+                    graphics.DrawLine(pen, 0, scr_y / 2, scr_x, scr_y / 2);
+                    graphics.DrawString(lines[0], font, new SolidBrush(frontColour.Color), scr_x / 4, scr_y / 4, sf);
+                    graphics.DrawString(lines[1], font, new SolidBrush(frontColour.Color), scr_x * 3 / 4, scr_y / 4, sf);
+                    graphics.DrawString(lines[2], font, new SolidBrush(frontColour.Color), scr_x / 4, scr_y * 3 / 4, sf);
+                    graphics.DrawString(lines[3], font, new SolidBrush(frontColour.Color), scr_x * 3 / 4, scr_y * 3 / 4, sf);
+                }
+                catch
+                {
+                    MessageBox.Show("Insufficient input.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            graphics.Dispose();
+
+
+            saveAndSet(img);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Wallpaper.Set(new Uri(Application.StartupPath + "\\layout.bmp"), Wallpaper.Style.Centered);
+        }
+    }
+}
